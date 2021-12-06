b11e6f0e8e1e423b3f3d39af04323ab9e84029ac
Songhareem <ignisilva92@gmail.com>
2021-12-06 17:35:27 +0900

    feat: starbucks/ header badges

--- package/starbucks/js/main.js
+++ package/starbucks/js/main.js
@@ -1,3 +1,4 @@
+// Header search bar 처리
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
@@ -14,3 +15,30 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+// Header badge 일정 이상 스크롤시, 투명화 처리
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    // _.throttle(함수, 시간)
+    // event가 너무 많이 발생되어 생기는 부하를 줄여줌
+    // throttle n초마다 익명함수를 실행
+    console.log("scroll");
+    if (window.scrollY > 800) {
+      // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
