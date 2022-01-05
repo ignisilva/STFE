e4e6e7d001384d23369257f7bc98dc0cedb26fdd
Songhareem <ignisilva92@gmail.com>
2022-01-05 15:53:47 +0900

    webpack connect static-files

--- package/webpack/webpack.config.js
+++ package/webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   // 파일을 읽어들이기 시작하는 진입점 설정
@@ -16,6 +17,13 @@ module.exports = {
   plugins: [
     new HtmlPlugin({
       template: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "static",
+        },
+      ],
     }),
   ],
 
