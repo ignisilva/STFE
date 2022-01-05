409fc4fbc066db09e553881eaf39f5b03f5adc9c
Songhareem <ignisilva92@gmail.com>
2022-01-05 15:39:00 +0900

    webpack add html plugin for show app in browser

--- package/webpack/webpack.config.js
+++ package/webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // 파일을 읽어들이기 시작하는 진입점 설정
@@ -9,5 +10,16 @@ module.exports = {
     // path: path.resolve(__dirname, "dist"), // path 옵션은 반드시 절대 경로 명시, default는 dist
     // filename: "main.js", // default는 main.js
     clean: true, // 빌드시, 기존에 생성된 dist 제거 후 재 생성
+  },
+
+  // 번들링 후, 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+  ],
+
+  devServer: {
+    host: "localhost",
   },
 };
