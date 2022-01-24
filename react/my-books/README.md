6229a9e5e5206b64bb0a3dd5b2bdd717b90ab638
Songhareem <ignisilva92@gmail.com>
2022-01-24 17:15:57 +0900

    feat: react toy project2, set error boundary

--- react/my-books/README.md
+++ react/my-books/README.md
@@ -7,6 +7,10 @@
 - $ npm i react-router-dom@5.2.1
 - $ npm i --save-dev @types/react-router-dom
 
+- $ npm i react-error-boundary
+  - componentDidPatch를 이용해서, error 발생시, error 페이지로 이동 시켜줌
+
 # 개발 순서
 
 - router 설정
+- error bounder 설정
--- react/my-books/src/App.tsx
+++ react/my-books/src/App.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Add from "./pages/Add";
 import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Signin from "./pages/Signin";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/edit/:id" component={Edit} />
-        <Route exact path="/book/:id" component={Detail} />
-        <Route exact path="/add" component={Add} />
-        <Route exact path="/signin" component={Signin} />
-        <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/edit/:id" component={Edit} />
+          <Route exact path="/book/:id" component={Detail} />
+          <Route exact path="/add" component={Add} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
